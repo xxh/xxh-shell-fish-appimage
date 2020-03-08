e9884ddebebab50483dc2b9e67a77d0425af37c9
anki-code <anki-code>
2020-03-08 19:06:45 +0300

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,18 +14,19 @@ CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $CURRENT_DIR
 
 # Check FUSE support
-check_result=`./fish --version 2>&1`
 if [[ ! -f .fish-check-done ]]; then
-  if [[ $check_result == *"FUSE"* ]]; then
+  check_result=`./fish --version 2>&1`
+  if [[ $check_result != *"version"* ]]; then
     # TODO: Extract AppImage if FUSE is unsupported
     #./fish --appimage-extract > /dev/null
     #mv squashfs-root fish-squashfs
     #mv fish fish-disabled
     #ln -s ./fish-squashfs/usr/bin/fish fish
-    echo "AppImage is unsupported on host:"
+    echo "Something went wrong:"
     echo $check_result
+  else
+    echo $check_result > .fish-check-done
   fi
-  echo $check_result > .fish-check-done
 fi
 
 ./fish # TODO: $EXECUTE_FILE $EXECUTE_COMMAND $VERBOSE
