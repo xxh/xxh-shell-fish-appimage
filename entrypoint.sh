b28e07666d737902c8cb074055a032773478bc4e
anki-code <anki-code>
2020-03-25 09:49:41 +0300

    libs fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,69 @@
 #!/bin/bash
 
-#while getopts f:c:v: option
-#do
-#case "${option}"
-#in
-#f) EXECUTE_FILE=${OPTARG};;
-#c) EXECUTE_COMMAND=${OPTARG};;
-#v) VERBOSE=${OPTARG};;
-#esac
-#done
+while getopts f:c:C:v:e:b: option
+do
+case "${option}"
+in
+f) EXECUTE_FILE=${OPTARG};;
+c) EXECUTE_COMMAND=${OPTARG};;
+C) EXECUTE_COMMAND_B64=${OPTARG};;
+v) VERBOSE=${OPTARG};;
+e) ENV+=("$OPTARG");;
+b) EBASH+=("$OPTARG");;
+esac
+done
+
+if [[ $VERBOSE != '' ]]; then
+  export XXH_VERBOSE=$VERBOSE
+fi
+
+for eb in "${EBASH[@]}"; do
+  bash_command=`echo $eb | base64 -d`
+
+  if [[ $XXH_VERBOSE == '1' || $XXH_VERBOSE == '2' ]]; then
+    echo Entrypoint bash execute: $bash_command
+  fi
+  eval $bash_command
+done
+
+if [[ $EXECUTE_COMMAND_B64 ]]; then
+  EXECUTE_COMMAND=`echo $EXECUTE_COMMAND_B64 | base64 -d`
+  if [[ $XXH_VERBOSE == '1' || $XXH_VERBOSE == '2' ]]; then
+    echo Execute command base64: $EXECUTE_COMMAND_B64
+    echo Execute command: $EXECUTE_COMMAND
+  fi
+
+  EXECUTE_COMMAND=(-c "${EXECUTE_COMMAND}")
+fi
 
 CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $CURRENT_DIR
+
+export XXH_HOME=`realpath $CURRENT_DIR/../../../..`
 
 # Check FUSE support
 if [[ ! -f .entrypoint-check-done ]]; then
   check_result=`./fish --version 2>&1`
   if [[ $check_result != *"version"* ]]; then
     if ! command -v fusermount >/dev/null 2>&1 ; then
-      echo "Now extracting the AppImage."
+      if [[ $XXH_VERBOSE == '1' || $XXH_VERBOSE == '2' ]]; then
+        echo "Extracting fish AppImage."
+      fi
+
       ./fish --appimage-extract > /dev/null
       mv squashfs-root fish-squashfs
       mv fish fish-disabled
       ln -s ./fish-squashfs/usr/bin/fish fish
     fi
-    echo "Something went wrong:"
-    echo $check_result
-  else
-    echo $check_result > .entrypoint-check-done
+  fi
+  echo $check_result > .entrypoint-check-done
+fi
+
+if [[ -d $XXH_HOME/xxh/shells/xxh-shell-fish-appimage/build/fish-squashfs ]]; then
+  export LD_LIBRARY_PATH=$XXH_HOME/xxh/shells/xxh-shell-fish-appimage/build/fish-squashfs/lib64:$XXH_HOME/xxh/shells/xxh-shell-fish-appimage/build/fish-squashfs/usr/lib64:$LD_LIBRARY_PATH
+  if [[ $XXH_VERBOSE == '2' ]]; then
+    echo "Added fish AppImage libs to LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
   fi
 fi
 
-./fish # TODO: $EXECUTE_FILE $EXECUTE_COMMAND $VERBOSE
+./fish "${EXECUTE_COMMAND[@]}"  # TODO: $EXECUTE_FILE
