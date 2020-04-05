e399ff6960bc7cf2bbaf31534c0cac2d33eed6c2
anki-code <anki-code>
2020-04-05 10:25:23 +0300

    prerun

--- entrypoint.sh
+++ entrypoint.sh
@@ -108,7 +108,7 @@ if [[ -d $XXH_HOME/.xxh/shells/xxh-shell-fish-appimage/build/fish-squashfs ]]; t
   fi
 fi
 
-for pluginrc_file in $(find $CURRENT_DIR/../../../plugins/xxh-plugin-*/build -type f -name '*pluginrc_prerun.sh' -printf '%f\t%p\n' 2>/dev/null | sort -k1 | cut -f2); do
+for pluginrc_file in $(find $CURRENT_DIR/../../../plugins/xxh-plugin-*/build -type f -name '*prerun.sh' -printf '%f\t%p\n' 2>/dev/null | sort -k1 | cut -f2); do
   if [[ -f $pluginrc_file ]]; then
     if [[ $XXH_VERBOSE == '1' ]]; then
       echo Load plugin $pluginrc_file
